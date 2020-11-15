5ddea8f42125a230521f40ee6fa294fb945e7e91
ibhuiyan <ibhuiyan@umich.edu>
2020-11-15 15:27:38 -0500

    tested out functions.

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,9 +1,36 @@
 const functions = require('firebase-functions');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const firebaseAdmin = require('firebase-admin');
+firebaseAdmin.initializeApp();
+
+const app = express();
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+app.get('/', async (req, res) => {
+    const snapshot = await firebaseAdmin.firestore().collection('courses').get();
+
+    let courses = [];
+    snapshot.forEach(doc => {
+        // console.log(doc.data());
+        courses.push(doc.data())
+    });
+    res.status(200).send(JSON.stringify(courses));
+});
+
+app.post('/', async (req, res) => {
+    const course = req.body;
+
+    await firebaseAdmin.firestore().collection('courses').doc(course.code).set(course);
+
+    res.status(200).send();
+});
+
+exports.courses = functions.https.onRequest(app)

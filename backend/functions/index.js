43bef0daa308431951db6c49dea3943fdc03413e
ibhuiyan <ibhuiyan@umich.edu>
2020-11-16 15:48:50 -0500

    Login endpoint.

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const crypto = require('crypto');
 
 const firebase = require('firebase-admin');
+const { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } = require('constants');
 firebase.initializeApp();
 
 const app = express();
@@ -20,16 +21,18 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 /* ---------------------------------------- API Routes ---------------------------------------- */
 /* ******************************************************************************************** */
 
-/* ---------------------------------------- Login ---------------------------------------- */
+/* ---------------------------------------- Accounts ---------------------------------------- */
 
+/* Create user if the username isn't already taken */
 app.post('/create-user', async (req, res) => {
-    const {username, password} = req.body;
+    const {
+        username,
+        password
+    } = req.body;
 
     // check that user doesn't already exist
     const usersRef = firebase.firestore().collection('users');
     const doc = await usersRef.doc(username).get();
-    // const dbResponse = await usersRef.where('username', '==', username).get();
-
     if (!doc.exists) {
         console.log('username not already in db, creating user');
 
@@ -43,14 +46,44 @@ app.post('/create-user', async (req, res) => {
             'password': hashedPassword,
             'courses': []
         });
-
-        res.status(201).send();
-        
+        res.status(201).send();   
     } else {
         console.log('username already exists in db');
         res.status(403).send();
     }
 
+});
+
+/* Login user by passing back userId */
+app.get('/login', async (req, res) => {
+    const {
+        username,
+        password
+    } = req.body;
+    
+    // TODO: change this later
+    const hashedPassword = password;
+
+    const usersRef = firebase.firestore().collection('users');
+    const doc = await usersRef.doc(username).get();
+    if (!doc.exists) {
+        console.log('username not found');
+        res.status(404).send();
+        return;
+    }
+    
+    const {
+        userId,
+        password: storedPassword
+    } = doc.data();
+    if (hashedPassword === storedPassword) {
+        res.status(200).send({
+            "username": username,
+            "userId": userId
+        });
+    } else {
+        res.status(401).send();
+    }
 });
 
 
